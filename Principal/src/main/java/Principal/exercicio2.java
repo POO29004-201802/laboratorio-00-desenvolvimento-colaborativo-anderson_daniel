77eaad6cb2b073664c6c47bd4d5e25a701d4c971
Anderson Gaspar <andersongasparm@gmail.com>
2018-08-20 21:16:27 -0300

    exe2

--- Principal/src/main/java/Principal/exercicio2.java
+++ Principal/src/main/java/Principal/exercicio2.java
@@ -1,5 +1,57 @@
 package Principal;
 public class exercicio2 {
-
+    public static void losango(int aux) {
+        int cont = 0;
+        String ast  = "*", esp  = " ";        String saida;
+        int media = aux/2, resto = aux%2;
+        int zero = 0;
+        if(aux==1){
+            System.out.println("*");
+        }else{
+            while(cont != aux){
+                saida="";
+                if(cont>media){
+                    zero = cont - media;
+                } else{
+                    if(resto !=0) {
+                        zero = media - cont;
+                    }else{
+                        zero = media - cont - 1;
+                        if(cont==media){
+                            zero = 0;
+                        }
+                    }
+                }
+                int cont2 = zero;
+                while(cont2 != 0) {
+                    saida = saida + esp;
+                    cont2 = cont2 - 1;
+                }
+                saida = saida + ast;
+                int auxi = 0;
+                if(resto!=0) {
+                    auxi = 2*media;
+                }else{
+                    auxi = 2*(media-1);
+                }
+                if((cont == 0) || (cont == (aux-1))) {
+                    auxi=auxi-(2*zero);
+                }else{
+                    auxi=(auxi-1)-(2*zero);
+                }
+                while(auxi != 0) {
+                    saida = saida + esp;
+                    auxi = auxi - 1;
+                }
+                if((cont == 0) || (cont == (aux-1))) {
+                    saida = saida;
+                }else{
+                    saida= saida + ast;
+                }
+                System.out.println(saida);
+                cont = cont + 1;
+            }
+        }
+    }
 }
 
--- Principal/src/main/java/Principal/main.java
+++ Principal/src/main/java/Principal/main.java
@@ -11,5 +11,11 @@ public class main {
         exe1 = new exercicio1();
         exercicio1.quadrado(aux1);
 
+        System.out.println("Insira a altura do losango: ");
+        aux = teclado.nextLine();
+        aux1 = Integer.parseInt(aux);
+        exercicio2 exe2;
+        exe2 = new exercicio2();
+        exercicio2.losango(aux1);
     }
 }
